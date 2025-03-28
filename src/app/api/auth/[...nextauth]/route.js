78d29e702af52dd1a6da87f839afd2cd23312c27
unknown <bibektotol@gmail.com>
2025-03-28 13:35:43 +0600

    added jwt

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -35,11 +35,7 @@ const handler = NextAuth({
       }
     }),
     
-    // EmailProvider({
-    //   server: process.env.EMAIL_SERVER,
-    //   from: process.env.EMAIL_FROM,
-      
-    // }),
+    
 
   ],
   pages: {
